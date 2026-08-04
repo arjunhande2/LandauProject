1ea9a2884066ea14ff7606e8ca9654506e22a3ad
arjunhande2 <arjunhande2@gmail.com>
2026-08-04 14:13:31 -0400

    Update firebase-config.js
    Removed API keys

--- firebase-config.js
+++ firebase-config.js
@@ -7,14 +7,7 @@
  * Firebase web configuration values identify the project; access is protected
  * by Firebase Authentication and the Firestore security rules in this project.
  */
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDqsZrtUQXhy7WkOt8gQa6I51BJaltCVWI',
-  authDomain: 'scannerdatabasegage.firebaseapp.com',
-  projectId: 'scannerdatabasegage',
-  storageBucket: 'scannerdatabasegage.firebasestorage.app',
-  messagingSenderId: '1017600933078',
-  appId: '1:1017600933078:web:aa2abe62e28f7b60049d77'
-};
+
 
 /* Database collection settings used by database.html. */
 export const databaseSettings = {
